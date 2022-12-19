36051b241b86ddaefe183a5bd2fdea3e197e6a35
Clara <clara_ljm@hotmail.com>
2022-12-19 11:50:42 -0300

    entity user and REST working

--- src/main/java/com/claramoreira/workshopmongo/WorkshopMongoApplication.java
+++ src/main/java/com/claramoreira/workshopmongo/WorkshopMongoApplication.java
@@ -1,4 +1,4 @@
-package com.claramoreira;
+package com.claramoreira.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
